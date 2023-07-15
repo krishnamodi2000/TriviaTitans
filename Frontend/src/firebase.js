ca9651ccc7ece77a372a550801f6d2e8ed431f44
Prabhleen Kaur <pr894105@dal.ca>
2023-07-14 23:22:27 -0300

    Added backend changes for Authentication module

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyARJQ4FbirWPTXOdk0Un54y2r74oc09h8E",
-    authDomain: "lab1-386517.firebaseapp.com",
-    projectId: "lab1-386517",
-    storageBucket: "lab1-386517.appspot.com",
-    messagingSenderId: "582868506312",
-    appId: "1:582868506312:web:09acfa977ca4af0a78b7fd",
-    measurementId: "G-9ZG01WQCC4"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 // Initialize Firebase app
